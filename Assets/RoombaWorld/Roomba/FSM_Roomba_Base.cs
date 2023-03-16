e87d056e723a92232322b999fbb47710dea541be
igarciava <igarciava@edu.tecnocampus.cat>
2023-03-16 21:59:23 +0100

    roomba dust funciona goood

--- Assets/RoombaWorld/Roomba/FSM_Roomba_Base.cs
+++ Assets/RoombaWorld/Roomba/FSM_Roomba_Base.cs
@@ -1,6 +1,5 @@
 using FSMs;
 using UnityEngine;
-using Steerings;
 
 [CreateAssetMenu(fileName = "FSM_Roomba_Base", menuName = "Finite State Machines/FSM_Roomba_Base", order = 1)]
 public class FSM_Roomba_Base : FiniteStateMachine
@@ -10,6 +9,8 @@ public class FSM_Roomba_Base : FiniteStateMachine
      * For instance: steering behaviours, blackboard, ...*/
     private ROOMBA_Blackboard blackboard;
     private GoToTarget goToTarget;
+    private GameObject theDust;
+    private GameObject theLastDust;
 
     public override void OnEnter()
     {
@@ -36,36 +37,82 @@ public class FSM_Roomba_Base : FiniteStateMachine
         /* STAGE 1: create the states with their logic(s)*/
 
         State Patrolling = new State("Patrolling",
-            () => { 
+            () =>
+            {
                 goToTarget.target = SensingUtils.FindRandomInstanceWithinRadius(gameObject, "PATROLPOINT", blackboard.patrolPointRadius);
                 goToTarget.enabled = true;
             }, // write on enter logic inside {}
             () => { }, // write in state logic inside {}
-            () => {
+            () =>
+            {
                 goToTarget.enabled = false;
                 goToTarget.target = null;
             }  // write on exit logic inisde {}  
+        );
+
+        State GoingToDust = new State("GoingToDust",
+            () =>
+            {
+                goToTarget.target = theLastDust;
+                goToTarget.enabled = true;
+            }, // write on enter logic inside {}
+            () => { }, // write in state logic inside {}
+            () =>
+            {
+                goToTarget.enabled = false;
+                goToTarget.target = null;
+            }  // write on exit logic inisde {}  
+        );
+
+        State Cleaning = new State("Cleaning",
+            () =>
+            {
+                Destroy(theLastDust);
+            }, // write on enter logic inside {}
+            () => { }, // write in state logic inside {}
+            () => { }  // write on exit logic inisde {}  
         );
 
 
         /* STAGE 2: create the transitions with their logic(s)
          * ---------------------------------------------------*/
 
-        Transition ToAnotherPatrolPoint = new Transition("ToAnotherPatrolPoint",
-            () => {
+        Transition RouteTerminated = new Transition("ToAnotherPatrolPoint",
+            () =>
+            {
                 return goToTarget.routeTerminated();
             }, // write the condition checkeing code in {}
             () => { }  // write the on trigger code in {} if any. Remove line if no on trigger action needed
         );
 
+        Transition DustDetected = new Transition("DustDetected",
+            () =>
+            {
+                theDust = SensingUtils.FindInstanceWithinRadius(gameObject, "DUST", blackboard.dustDetectionRadius);
+                blackboard.AddToMemory(theDust);
+                theLastDust = blackboard.RetrieveFromMemory();
+                return SensingUtils.DistanceToTarget(gameObject, theLastDust) < blackboard.dustDetectionRadius;
+            }, // write the condition checkeing code in {}
+            () => { }  // write the on trigger code in {} if any. Remove line if no on trigger action needed
+        );
 
+        Transition DustReached = new Transition("DustReached",
+            () =>
+            {
+                return SensingUtils.DistanceToTarget(gameObject, theLastDust) < blackboard.dustReachedRadius;
+            }, // write the condition checkeing code in {}
+            () => { }  // write the on trigger code in {} if any. Remove line if no on trigger action needed
+        );
 
 
         /* STAGE 3: add states and transitions to the FSM 
          * ----------------------------------------------*/
 
-        AddState(Patrolling);
-        AddTransition(Patrolling, ToAnotherPatrolPoint, Patrolling);
+        AddStates(Patrolling, GoingToDust, Cleaning);
+        AddTransition(Patrolling, RouteTerminated, Patrolling);
+        AddTransition(Patrolling, DustDetected, GoingToDust);
+        AddTransition(GoingToDust, DustReached, Cleaning);
+        AddTransition(Cleaning, RouteTerminated, Patrolling);
 
 
         /* STAGE 4: set the initial state*/
